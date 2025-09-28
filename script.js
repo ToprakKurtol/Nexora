2e0b8cc863b46a26efad76001586074a16f6bded
ToprakKurtol <toprakkurt300@gmail.com>
2025-09-28 13:02:28 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeText() {
+    document.querySelector("h1").textContent = "Site Çalışıyor! 🚀";
+    alert("Tebrikler, script başarıyla çalıştı!");
+}
+
+console.log("Script başarıyla yüklendi!");
